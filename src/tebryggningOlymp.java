0594ea0712a7ac947efc00fc69cab7c659419049
elloot <elloot@github.com>
2019-11-29 11:17:35 +0100

    start work on tebrygg

--- src/tebryggningOlymp.java
+++ src/tebryggningOlymp.java
@@ -1,7 +1,26 @@
+import java.util.Scanner;
+
 /**
  * Class description
  * 2019-11-29
  * Author: Elliot Duchek
  */
 public class tebryggningOlymp {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        System.out.print("Input number of bagos: ");
+        int numBag = kb.nextInt();
+        kb.nextLine();
+
+        System.out.print("Input number of ppl: ");
+        int numPpl = kb.nextInt();
+        kb.nextLine();
+
+        int[] bagReach = new int[numBag];
+
+        for (int i = 0; i<numBag; i++) {
+            System.out.print("How many cups of tea does bag #" + (i+1) + " make?");
+            bagReach[i] = kb.nextInt();
+        }
+    }
 }
