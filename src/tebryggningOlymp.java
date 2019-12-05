7547437414dac6a7dd11c36ee90b48d4b8691e75
elloot <elloot@github.com>
2019-12-05 10:24:23 +0100

    refractor tebryggning

--- src/tebryggningOlymp.java
+++ src/tebryggningOlymp.java
@@ -8,19 +8,32 @@ import java.util.Scanner;
 public class tebryggningOlymp {
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
-        System.out.print("Input number of bagos: ");
-        int numBag = kb.nextInt();
-        kb.nextLine();
+        int numBag = getNumBag(kb);
+        int numPpl = getNumPpl(kb);
+        int[] bagReach = getBagReach(kb, numBag);
+    }
 
-        System.out.print("Input number of ppl: ");
-        int numPpl = kb.nextInt();
-        kb.nextLine();
-
+    private static int[] getBagReach(Scanner kb, int numBag) {
         int[] bagReach = new int[numBag];
 
         for (int i = 0; i<numBag; i++) {
             System.out.print("How many cups of tea does bag #" + (i+1) + " make?");
             bagReach[i] = kb.nextInt();
         }
+        return bagReach;
+    }
+
+    private static int getNumPpl(Scanner kb) {
+        System.out.print("Input number of ppl: ");
+        int numPpl = kb.nextInt();
+        kb.nextLine();
+        return numPpl;
+    }
+
+    private static int getNumBag(Scanner kb) {
+        System.out.print("Input number of bagos: ");
+        int numBag = kb.nextInt();
+        kb.nextLine();
+        return numBag;
     }
 }
